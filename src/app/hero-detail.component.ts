a13e9006432e99c69e035cc3bf15cfb0d3100e52
Peter Dyce <dyce.peter@gmail.com>
2017-03-03 13:34:35 +1100

    set up the app-router.module - and it works.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -7,23 +7,10 @@ import { HeroService } from './hero.service';
 import 'rxjs/add/operator/switchMap';
 
 
-
-
 @Component({
-
-   selector: 'my-hero-detail',
-   template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-    <button (click)="goBack()">Back</button>
-  </div>
-`
-
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: './hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit {
